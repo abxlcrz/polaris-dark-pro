1aea98074be51730feac68dce0bf0aac2b9bd54e
Abel Cruz <abxlcrz@gmail.com>
2025-11-19 18:38:45 -0300

    fix: jsx content colour

---
+++ test-files/jsx-simple.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Test() {
+  return (
+    <div>
+      <p>Email: test@example.com</p>
+      <span>This is plain text</span>
+      <h1>Header text</h1>
+    </div>
+  );
+}
+
+export default Test;
